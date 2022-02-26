355c82a6aeed8f4983f652397040920cbe427307
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-25 18:01:04 -0700

    Testing

---
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruck {
+
+}
---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}

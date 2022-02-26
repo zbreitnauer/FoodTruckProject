5d5d5b9e228a2812902737918b82e724fb7e5b9e
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-25 19:55:15 -0700

    added prompts

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
-
+	
+	public int numericId;
+	public int numericRating;
+	public String truckName;
+	public String foodType;
+	
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,11 +1,23 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-
+		Scanner kb = new Scanner(System.in);
+		FoodTruck ft = new FoodTruck();
+		
+		System.out.println("Please enter the food truck name.");
+		ft.truckName = kb.nextLine();
+		
+		System.out.println("Please enter the type of food.");
+		ft.foodType = kb.nextLine();
+		
+		System.out.println("Please give a rating between 1 - 5.");
+		ft.numericRating = kb.nextInt();
+		
+		
 	}
 
 }

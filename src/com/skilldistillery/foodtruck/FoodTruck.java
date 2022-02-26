59ffecc668721f75737b019f5327836890de1f30
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-25 20:56:01 -0700

    added a menu

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -7,4 +7,17 @@ public class FoodTruck {
 	public String truckName;
 	public String foodType;
 	
+	 public void printMenu(){
+	      System.out.println();
+			System.out.println("========= MENU =========");
+			System.out.println("|                      |");
+			System.out.println("|   1. All food trucks |");
+			System.out.println("|   2. Average rating  |");
+			System.out.println("|   3. Highest rated   |");
+			System.out.println("|   4. Exit            |");
+			System.out.println("|                      |");
+			System.out.println("========================");
+			System.out.println();
+	
+}
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -5,19 +5,32 @@ import java.util.Scanner;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
+		boolean trucks = true;
 		Scanner kb = new Scanner(System.in);
 		FoodTruck ft = new FoodTruck();
 		
-		System.out.println("Please enter the food truck name.");
-		ft.truckName = kb.nextLine();
-		
-		System.out.println("Please enter the type of food.");
-		ft.foodType = kb.nextLine();
-		
-		System.out.println("Please give a rating between 1 - 5.");
-		ft.numericRating = kb.nextInt();
-		
-		
-	}
+//		while (trucks) {
+			System.out.println("Please enter the food truck name.");
+			ft.truckName = kb.nextLine();
 
+			if (ft.truckName.equals("Quit")) {
+				System.out.println("You've chosen to quit.");
+//				break;
+		}
+			System.out.println("Please enter the type of food.");
+			ft.foodType = kb.nextLine();
+
+			System.out.println("Please give a rating between 1 - 5.");
+			ft.numericRating = kb.nextInt();
+
+//		}
+	
+		ft.printMenu();
+	
+	
+	
+	
+	}
+	
+	
 }

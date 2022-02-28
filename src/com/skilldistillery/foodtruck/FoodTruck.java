d9375f361785b4b9bdf187654823c2064182ac07
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-27 17:36:56 -0700

    added getter/setters

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,23 +1,88 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
-	
+
 	public int numericId;
 	public int numericRating;
 	public String truckName;
 	public String foodType;
-	
-	 public void printMenu(){
-	      System.out.println();
-			System.out.println("========= MENU =========");
-			System.out.println("|                      |");
-			System.out.println("|   1. All food trucks |");
-			System.out.println("|   2. Average rating  |");
-			System.out.println("|   3. Highest rated   |");
-			System.out.println("|   4. Exit            |");
-			System.out.println("|                      |");
-			System.out.println("========================");
-			System.out.println();
-	
-}
+	public static int nextNumericId = 5;
+
+	public FoodTruck() {
+	}
+
+	public FoodTruck(String truckName, String foodType, int numericRating, int numericId) {
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.numericRating = numericRating;
+		this.numericId = numericId;
+	}
+
+	public String gettruckName() {
+		return truckName;
+	}
+
+	public void settruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public String getfoodType() {
+		return foodType;
+	}
+
+	public void setfoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getnumericRating() {
+		return numericRating;
+	}
+
+	public void setnumericRating(int numericRating) {
+		this.numericRating = numericRating;
+	}
+
+	public int getnumericId() {
+		return numericId;
+	}
+
+	public void setnumericId(int truckID) {
+		this.numericId = numericId + nextNumericId;
+	}
+
+	public static int getNumericId() {
+		return nextNumericId;
+	}
+
+	public static void setnextNumericId(int nextNumericId) {
+		FoodTruck.nextNumericId = nextNumericId;
+		nextNumericId++;
+	}
+
+	public String toString() {
+		StringBuilder make = new StringBuilder();
+		make.append("Food Truck Name: ").append(truckName).append(", Food Type: ").append(foodType).append(", Rating: ")
+				.append(numericRating).append(", and truckID: ").append(numericId).append(".");
+
+		return make.toString();
+	}
+
+	public void displayCar() {
+		String carData = toString();
+		System.out.println(carData);
+	}
+
+	public void printMenu() {
+		System.out.println();
+		System.out.println("========= MENU =========");
+		System.out.println("|                      |");
+		System.out.println("|   1. All food trucks |");
+		System.out.println("|   2. Average rating  |");
+		System.out.println("|   3. Highest rated   |");
+		System.out.println("|   4. Exit            |");
+		System.out.println("|                      |");
+		System.out.println("========================");
+		System.out.println();
+
+	}
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -6,31 +6,50 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		boolean trucks = true;
+		int choice;
 		Scanner kb = new Scanner(System.in);
 		FoodTruck ft = new FoodTruck();
-		
-//		while (trucks) {
-			System.out.println("Please enter the food truck name.");
-			ft.truckName = kb.nextLine();
 
-			if (ft.truckName.equals("Quit")) {
-				System.out.println("You've chosen to quit.");
-//				break;
+		while (trucks) {
+		System.out.println("Please enter the food truck name.");
+		ft.truckName = kb.nextLine();
+		if (ft.truckName.equals("Quit")) {
+			System.out.println("You've chosen to quit.");
+				break;
 		}
-			System.out.println("Please enter the type of food.");
-			ft.foodType = kb.nextLine();
+		System.out.println("Please enter the type of food.");
+		ft.foodType = kb.nextLine();
 
-			System.out.println("Please give a rating between 1 - 5.");
-			ft.numericRating = kb.nextInt();
+		System.out.println("Please give a rating between 1 - 5.");
+		ft.numericRating = kb.nextInt();
 
-//		}
-	
-		ft.printMenu();
-	
-	
-	
-	
+		}
+
+		while (trucks) {
+
+			ft.printMenu();
+			System.out.print("Which would you like to do? ");
+			choice = kb.nextInt();
+
+			switch (choice) {
+
+			case 1:
+				ft.displayCar();
+
+				break;
+			case 2:
+				System.out.println(ft.foodType);
+				break;
+			case 3:
+				System.out.println(ft.numericRating);
+
+				break;
+			case 4:
+				System.out.print("You chosen to quit");
+				return;
+
+			}
+
+		}
 	}
-	
-	
 }
